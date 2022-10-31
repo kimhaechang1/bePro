aedad71c66d88f420f34bb9405a864e2f244ca94
kimhaechang1 <81299056+kimhaechang1@users.noreply.github.com>
2022-10-31 21:31:03 +0900

    20221031 상단바 검색버튼 추가 및 사이드바(해시태그순위) 프레임 제작

---
+++ client/src/components/SideBar.js
@@ -0,0 +1,21 @@
+
+function SideBar(){
+    return(
+        <div className="sideBarFrame">
+              <div className="sideBarTitle"># 실시간 태그순위</div>
+              <div className="sideBarContentFrame">
+                <div>1. # 컴퓨터공학과</div>
+                <div>2. # 컴퓨터공학과</div>
+                <div>3. # 컴퓨터공학과</div>
+                <div>4. # 컴퓨터공학과</div>
+                <div>5. # 컴퓨터공학과</div>
+                <div>6. # 컴퓨터공학과</div>
+                <div>7. # 컴퓨터공학과</div>
+                <div>8. # 컴퓨터공학과</div>
+                <div>9. # 컴퓨터공학과</div>
+                <div>10. # 컴퓨터공학과</div>
+              </div>
+        </div>
+    )
+}
+export default SideBar;
--- client/src/views/MainPage.js
+++ client/src/views/MainPage.js
@@ -3,22 +3,30 @@ import '../components/css/SignIn.css'
 import '../components/css/Card.css';
 import '../components/css/Header.css';
 import '../components/css/SignUp.css';
+import '../components/css/SideBar.css';
 import Header from '../components/Header';
 import Card from '../components/Card';
-
+import SideBar from '../components/SideBar';
 function MainPage(){
     return(
     <div className="App">
       <Header/>
       <div className="mainContentFrame">
         <div className="innerArea border">
-          <div className="cardArea">
-            <Card name={"최신 QnA"}/>
-            <Card name={"최근 등록된 용어"}/>
-          </div>
-          <div className="cardArea">
-            <Card name={"공지사항"}/>
-            <Card name={"조회수 높은 순"}/>
+          <div className="contentFrame">
+            {/*사이드 바 영역*/}
+            <SideBar/>
+            {/*사이드 바 영역 끝*/}
+            <div className="cardFrame">
+              <div className="cardArea">
+                <Card name={"최신 QnA"}/>
+                <Card name={"최근 등록된 용어"}/>
+              </div>
+              <div className="cardArea">
+                <Card name={"공지사항"}/>
+                <Card name={"조회수 높은 순"}/>
+              </div>  
+            </div>
           </div>
         </div>
       </div>
