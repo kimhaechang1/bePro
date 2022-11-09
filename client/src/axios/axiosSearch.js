17e14f98485665dedb53e09f2d685031e6170c34
kimhaechang <81299056+kimhaechang1@users.noreply.github.com>
2022-11-09 15:40:36 +0900

    20221109 Add SearchBoxComponent & Search function
    MainPage Search Input-tag be changed to Component(SearchBox.js) and Search function be added

--- client/src/axios/axiosSearch.js
+++ client/src/axios/axiosSearch.js
@@ -1,4 +1,19 @@
-function axiosSearch(){
-    
+import axios from "axios";
+const axiosSearch = (inputValue, hashTags=[]) =>{
+    if(inputValue.length<2 && hashTags.length<1){
+        alert("2글자 이상 혹은 태그를 선택 해 주세요");
+    }else{
+        let data ={
+            inputValue : inputValue,
+            hashTags : hashTags
+        }
+        const request = axios.post('/search',data)
+        .then(response => 
+            response)
+        
+        return request
+    }
 }
+    
+
 export default axiosSearch;
